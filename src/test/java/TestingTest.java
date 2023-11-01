00b9d27adbcda7553ee533f73c15a17b9dc27009
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2023-11-01 13:42:03 +0100

    Testing class erstellt

---
+++ src/test/java/TestingTest.java
@@ -0,0 +1,22 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Test;
+public class TestingTest {
+
+@Test
+public void multiplyTest_when5Ties6_thenReturn30(){
+    //GIVEN
+    int value1 = 5;
+    int value2 = 6;
+
+    //WHEN
+    int actual = Testing.multiply(value1,value2);
+
+    //THEN
+
+    assertEquals(30,actual)
+
+}
+
+}
