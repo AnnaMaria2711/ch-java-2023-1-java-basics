5a37fd897302f49ed76ad24841eb595c10d5c7ca
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2023-11-01 16:16:38 +0100

    Testing Aufgaben gelöst

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -5,4 +5,31 @@ public class Testing {
     public static int multiply(int valueA, int valueB){
         return valueA * valueB;
     }
+
+    public static int divide(int valA, int valB){
+        return valA / valB;
+    }
+
+    public static int add(int value1,int value2){
+        return value1 + value2;
+    }
+
+    public static boolean evenNumber(int valueC) {
+        if (valueC % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static String lowerCaseToUpperCase(String s){
+        return s.toUpperCase();
+    }
+    public static boolean checkForPositive(double valueE) {
+        if (valueE > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
--- src/test/java/TestingTest.java
+++ src/test/java/TestingTest.java
@@ -5,19 +5,119 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestingTest {
 
-@Test
-public void multiplyTest_when5Ties6_thenReturn30(){
-    //GIVEN
-    int value1 = 5;
-    int value2 = 6;
+    @Test
+    public void multiplyTest_when5Ties6_thenReturn30() {
+        //GIVEN
+        int value1 = 5;
+        int value2 = 6;
+        String a = "Hallo";
 
-    //WHEN
-    int actual = Testing.multiply(value1,value2);
 
-    //THEN
+        //WHEN
+        int actual = Testing.multiply(value1, value2);
 
-    assertEquals(30,actual);
+        //THEN
 
-}
+        assertEquals(30, actual);
 
+    }
+
+    @Test
+    public void divideTest_when27DividedBy9_return3() {
+        // GIVEN
+        int value1 = 27;
+        int value2 = 9;
+
+
+        // WHEN
+        int actual = Testing.divide(value1, value2);
+
+
+        // THEN
+        assertEquals(3, actual);
+
+    }
+
+    @Test
+
+    public void addTest_when77AddedTo22_return99() {
+        // GIVEN
+
+        int value1 = 77;
+        int value2 = 22;
+
+        // WHEN
+
+        int actual = Testing.add(value1, value2);
+
+
+        // THEN
+
+        assertEquals(99, actual);
+    }
+
+    @Test
+    public void evenNumberTest_whenNumberIsEven_returnTrue() {
+        // GIVEN
+
+        int value1 = 60;
+
+        // WHEN
+
+        boolean shouldBeTrue = Testing.evenNumber(value1);
+
+        // THEN
+
+        assertTrue(shouldBeTrue);
+    }
+
+    @Test
+    public void lowerCaseToUpperCaseTest_whenStringConvertedToUppercase_ReturnTrue() {
+
+        // GIVEN
+
+        String value1 = "helloooo!";
+
+        // WHEN
+
+        String actual = Testing.lowerCaseToUpperCase(value1);
+
+        // THEN
+
+        assertEquals("HELLOOOO!", actual);
+
+    }
+
+    @Test
+    public void checkForPositiveTest_ifNumberPositive_returnTrue() {
+
+        // GIVEN
+
+        double value1 = 44.33;
+
+        // WHEN
+
+        boolean actual = Testing.checkForPositive(value1);
+
+        // THEN
+
+        assertTrue(actual);
+    }
+
+    @Test
+
+    public void checkForPositiveTest_ifNumberPositive_returnFalse() {
+
+        // GIVEN
+
+        double value1 = 44.33;
+
+        // WHEN
+
+        boolean actual = Testing.checkForPositive(value1);
+
+        // THEN
+
+        assertFalse(actual);
+    }
 }
