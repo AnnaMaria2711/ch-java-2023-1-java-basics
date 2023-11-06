c851478f52a040006f16305ad8f5bec0bfaf6631
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2023-11-06 11:42:21 +0100

    FizzBuzzTest formatiert

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,7 +3,7 @@ import org.example.FizzBuzz;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-public class FizzBuzzTest {
+    public class FizzBuzzTest {
 
 
     @Test
