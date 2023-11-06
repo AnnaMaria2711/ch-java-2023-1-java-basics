fffc28770b15f089eed23ed14c773943d330bac4
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2023-11-06 11:41:41 +0100

    Spielbrett und SpielbrettTest added and edited

---
+++ src/main/java/org/example/Spielbrett.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Spielbrett {
+
+    // public static int[][](int borard){
+
+    //}
+    //int[] [] board = new int[5][5]
+
+    public static int getX() {
+        return 0;
+    }
+
+}
---
+++ src/test/java/SpielbrettTest.java
@@ -0,0 +1,40 @@
+import org.example.FizzBuzz;
+
+import org.example.Spielbrett;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpielbrettTest {
+
+    @Test
+    void SpielbrettTest_whenWpressed_moveUp() {
+        // GIVEN
+
+        int[][] board = new int[5][5];
+
+
+        // WHEN
+
+        //   int[] [] actual = Spielbrett.moveUp;
+
+        // THEN
+
+        //   assertArrayEquals(board[1][2], actual);
+    }
+
+    @Test
+    void getXTest_shouldReturn0() {
+        // GIVEN
+
+
+        // WHEN
+
+        int actual = Spielbrett.getX();
+
+        // THEN
+
+        assertEquals(0, actual);
+    }
+}
